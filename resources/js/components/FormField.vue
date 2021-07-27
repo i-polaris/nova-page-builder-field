3d102342aebf300a4193a59f293e240f104b4ac5
julien romagnoli <julien.romagnoli@akktis.com>
2021-07-27 20:08:31 +0000

    adding newsletter et ckeditor

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -4,6 +4,8 @@
 
 <script>
 
+import './loader.js'
+import 'ckeditor';
 import grapesjs from 'grapesjs';
 import basicBlocks from 'grapesjs-blocks-basic';
 import pluginNavbar from 'grapesjs-navbar';
@@ -11,6 +13,7 @@ import pluginCountdown from 'grapesjs-component-countdown';
 import pluginForms from 'grapesjs-plugin-forms';
 import pluginExport from 'grapesjs-plugin-export';
 import pluginNewsletter from 'grapesjs-preset-newsletter';
+import pluginsCkEditor from 'grapesjs-plugin-ckeditor';
 // import pluginAviary from 'grapesjs-aviary';
 // import pluginFilestack from 'grapesjs-plugin-filestack';
 import custom from '../plugins/custom';
@@ -40,7 +43,7 @@ export default {
          * Fill the given FormData object with the field's internal value.
          */
         fill(formData) {
-            let newValue = '<style>' + this.editor.getCss() + '</style><div>' + this.editor.getHtml() + '</div>';
+            let newValue =  '<style>' + this.editor.getCss() + '</style>' + this.editor.getHtml();
             formData.append(this.field.attribute, newValue || '')
         },
 
@@ -53,43 +56,57 @@ export default {
     },
 
     mounted() {
-        this.editor = grapesjs.init({
-            container: '#editor',
-            storageManager: { autoload: 0 },
-            width: '100%',
-             plugins: [
-                 basicBlocks,
-                 // pluginAviary,
-                 pluginExport,
-                 pluginCountdown,
-                 // pluginFilestack,
-                 pluginForms,
-                 pluginNavbar,
-                 pluginNewsletter,
-                 custom
-             ],
-            styleManager : {
-                sectors: [
-                    {
-                        name: 'General',
-                        open: false,
-                        buildProps: ['float', 'display', 'position', 'top', 'right', 'left', 'bottom']
-                    },{
-                        name: 'Dimension',
-                        open: false,
-                        buildProps: ['width', 'height', 'max-width', 'min-height', 'margin', 'padding'],
-                    },{
-                        name: 'Typography',
-                        open: false,
-                        buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-shadow'],
-                    },{
-                        name: 'Decorations',
-                        open: false,
-                        buildProps: ['border-radius-c', 'background-color', 'border-radius', 'border', 'box-shadow', 'background'],
-                    }
-                ],
+
+        var editor;
+        this.editor = editor = grapesjs.init({
+            clearOnRender: true,
+            height: '600px',
+            storageManager: {
+                id: 'gjs-nl-',
             },
-        });
+            container : '#editor',
+            fromElement: true,
+            plugins: ['gjs-preset-newsletter', 'gjs-plugin-ckeditor'],
+        pluginsOpts: {
+          'gjs-preset-newsletter': {
+            modalLabelImport: 'Paste all your code here below and click import',
+            modalLabelExport: 'Copy the code and use it wherever you want',
+            codeViewerTheme: 'material',
+            //defaultTemplate: templateImport,
+            importPlaceholder: '<table class="table"><tr><td class="cell">Hello world!</td></tr></table>',
+            cellStyle: {
+              'font-size': '12px',
+              'font-weight': 300,
+              'vertical-align': 'top',
+              color: 'rgb(111, 119, 125)',
+              margin: 0,
+              padding: 0,
+            }
+          },'gjs-plugin-ckeditor' : {
+            
+
+                position: 'center',
+            options: {
+              startupFocus: true,
+              extraAllowedContent: '*(*);*{*}', // Allows any class and any inline style
+              allowedContent: true, // Disable auto-formatting, class removing, etc.
+              enterMode: CKEDITOR.ENTER_BR,
+              extraPlugins: 'sharedspace,justify,colorbutton,panelbutton,font',
+              toolbar: [
+                { name: 'styles', items: ['Font', 'FontSize' ] },
+                ['Bold', 'Italic', 'Underline', 'Strike'],
+                {name: 'paragraph', items : [ 'NumberedList', 'BulletedList']},
+                {name: 'links', items: ['Link', 'Unlink']},
+                {name: 'colors', items: [ 'TextColor', 'BGColor' ]},
+              ],
+            }
+
+            }
+        }
+      });
+
+        window.CKEDITOR.dtd.$editable.span = 1
+        window.CKEDITOR.dtd.$editable.a = 1
         this.editor.setComponents(this.field.value);
     }
 }
